d478fd560b5cb70460557d0185fdce17d508bbb8
Erik Ferreira <erik.souzagf9@gmail.com>
2021-05-28 17:30:55 -0300

    structure page orphanage-map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+
+import mapMarkerImg from "../images/map-marker.svg";
+
+import "../styles/orphanages-map.css";
+
 function OrphanagesMap() {
   return (
-    <div>
-      <span>OrphanagesMap</span>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
     </div>
   );
 }
