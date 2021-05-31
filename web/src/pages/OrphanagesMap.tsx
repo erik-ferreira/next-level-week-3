b31dde5d4dfafae0b4e53178fd6248de355cf5b1
Erik Ferreira <erik.souzagf9@gmail.com>
2021-05-30 21:44:13 -0300

    finish first date nlw

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 import mapMarkerImg from "../images/map-marker.svg";
 
 import "../styles/orphanages-map.css";
+import "leaflet/dist/leaflet.css";
 
 function OrphanagesMap() {
   return (
@@ -22,7 +24,22 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <MapContainer
+        center={[-3.797847, -38.5181961]}
+        zoom={15}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+        {/* <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        /> */}
+      </MapContainer>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
