43651c06b8acacbb971b610f4df75ed9b2517c6e
erik-ferreira <erikdesouzagf@gmail.com>
2021-07-15 23:58:27 -0300

    abstrect code

---
+++ backend/src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const routes = Router();
+
+import OrphanagesController from "./controllers/OrphanagesController";
+
+routes.post("/orphanages", OrphanagesController.create);
+
+export default routes;
